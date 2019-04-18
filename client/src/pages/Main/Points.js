eb1aacd84f5bdcdc4bbadb8309bcbd30430974b3
Enes Tüfekçi <enesxtufekci@gmail.com>
2019-04-18 10:14:56 +0200

    teams

--- client/src/pages/Main/Points.js
+++ client/src/pages/Main/Points.js
@@ -5,7 +5,7 @@ function Points({ teams }) {
     <div className="points-container">
       {teams.map(team => (
         <div key={team} className="team-container">
-          <div className="team-title">Takım {team.title}</div>
+          <div className="team-title">{team.title}</div>
           <div className="team-points">{team.points}</div>
           <p>Puan</p>
         </div>
